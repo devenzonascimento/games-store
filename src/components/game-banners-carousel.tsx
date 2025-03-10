83d2a4405c2ce99617d234d5c9a0f7fc6ea0c858
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-03-09 23:58:30 -0300

    feat: adicionar componente GameBannersCarousel e tipo Game para exibir jogos em carrossel

---
+++ src/components/game-banners-carousel.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { cn } from '@/lib/utils'
+import {
+  Carousel,
+  CarouselItem,
+  CarouselContent,
+  CarouselApi,
+} from '@/components/ui/carousel'
+import { GameBanner } from '@/components/game-banner'
+import { Game } from '@/types/game'
+
+type GameBannersCarouselProps = {
+  games: Game[]
+}
+
+export function GameBannersCarousel({ games }: GameBannersCarouselProps) {
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(0)
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap())
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap())
+    })
+  }, [api])
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Carousel setApi={setApi}>
+        <CarouselContent>
+          {games.map(game => (
+            <CarouselItem key={game.id}>
+              <GameBanner
+                imageUrl={game.imageUrl}
+                title={game.title}
+                description={game.description}
+                price={game.price}
+                platformsAvaliable={game.platformsAvaliable}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
+      {/* Elipssis points */}
+      <div className="w-full flex items-center justify-center gap-2">
+        {Array.from({ length: count }).map((_, index) => (
+          <div
+            key={index.toString()}
+            className={cn(
+              'size-2 rounded-full transition-all duration-500',
+              current === index ? 'bg-white' : 'bg-white/50',
+            )}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/types/game.ts
@@ -0,0 +1,14 @@
+export enum Platform {
+  Pc = 0,
+  PlayStation = 1,
+  Xbox = 2,
+}
+
+export type Game = {
+  id: number
+  title: string
+  description: string
+  price: number
+  platformsAvaliable: Platform[]
+  imageUrl: string
+}
