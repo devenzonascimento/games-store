d0269e7555c8aec659db1b0cfcc1e45df07cd9a6
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-03-20 01:29:39 -0300

    feat: adicionar componente GameVerticalCard e atualizar layout da página inicial

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { GameBannersCarousel } from '@/components/game-banners-carousel'
+import { GameVerticalCard } from '@/components/game-vertical-card'
 import { Logo } from '@/components/icons/logo'
 import { Sidebar } from '@/components/sidebar'
 import { Tag } from '@/components/tag'
@@ -19,7 +20,7 @@ export default function Home() {
         <Sidebar />
       </header>
 
-      <main className="flex-1 bg-zinc-900">
+      <main className="flex-1 bg-zinc-900 overflow-y-auto">
         <section className="p-4">
           <div className="p-2 flex items-center gap-2 bg-zinc-800 border border-zinc-600 rounded-xl">
             <SearchIcon />
@@ -39,10 +40,16 @@ export default function Home() {
           <GameBannersCarousel games={mockGameBanners} />
         </section>
 
-        <section className="w-full py-2 px-4 flex items-center gap-2 overflow-x-auto">
+        <section className="w-full py-2 px-4 grid grid-flow-col gap-2 overflow-x-auto no-scrollbar">
           <Tag title="Vencedor" Icon={TrophyIcon} isActive />
           <Tag title="Mais vendidos na semana" Icon={CalendarIcon} />
           <Tag title="Novidades" Icon={SparklesIcon} />
+        </section>
+
+        <section className="w-full py-2 px-4 grid grid-cols-2 gap-4 overflow-y-auto">
+          {mockGameCards.map(game => (
+            <GameVerticalCard key={game.id} game={game} />
+          ))}
         </section>
       </main>
     </div>
@@ -52,7 +59,7 @@ export default function Home() {
 const mockGameBanners = [
   {
     id: 1,
-    imageUrl: '/forza-horizon-5-banner.jpg',
+    imageUrl: '/forza-horizon-5-banner.png',
     title: 'Forza Horizon 5: Premium Edition',
     description:
       'Your Ultimate Horizon Adventure awaits! Explore the vibrant and ever-evolving open world landscapes of Mexico with limitless, fun driving action in hundreds of the world’s greatest cars. The Premium Edition includes early access to the full game, allowing you to play four days early beginning November 5, 2021. Also included are the Welcome Pack, Car Pass, VIP Membership, and two game expansions when they become available.',
@@ -61,7 +68,7 @@ const mockGameBanners = [
   },
   {
     id: 2,
-    imageUrl: '/forza-horizon-5-banner.jpg',
+    imageUrl: '/forza-horizon-5-banner.png',
     title: 'Forza Horizon 5: Premium Edition',
     description:
       'Your Ultimate Horizon Adventure awaits! Explore the vibrant and ever-evolving open world landscapes of Mexico with limitless, fun driving action in hundreds of the world’s greatest cars. The Premium Edition includes early access to the full game, allowing you to play four days early beginning November 5, 2021. Also included are the Welcome Pack, Car Pass, VIP Membership, and two game expansions when they become available.',
@@ -70,7 +77,7 @@ const mockGameBanners = [
   },
   {
     id: 3,
-    imageUrl: '/forza-horizon-5-banner.jpg',
+    imageUrl: '/forza-horizon-5-banner.png',
     title: 'Forza Horizon 5: Premium Edition',
     description:
       'Your Ultimate Horizon Adventure awaits! Explore the vibrant and ever-evolving open world landscapes of Mexico with limitless, fun driving action in hundreds of the world’s greatest cars. The Premium Edition includes early access to the full game, allowing you to play four days early beginning November 5, 2021. Also included are the Welcome Pack, Car Pass, VIP Membership, and two game expansions when they become available.',
@@ -79,7 +86,7 @@ const mockGameBanners = [
   },
   {
     id: 4,
-    imageUrl: '/forza-horizon-5-banner.jpg',
+    imageUrl: '/forza-horizon-5-banner.png',
     title: 'Forza Horizon 5: Premium Edition',
     description:
       'Your Ultimate Horizon Adventure awaits! Explore the vibrant and ever-evolving open world landscapes of Mexico with limitless, fun driving action in hundreds of the world’s greatest cars. The Premium Edition includes early access to the full game, allowing you to play four days early beginning November 5, 2021. Also included are the Welcome Pack, Car Pass, VIP Membership, and two game expansions when they become available.',
@@ -88,10 +95,57 @@ const mockGameBanners = [
   },
   {
     id: 5,
-    imageUrl: '/forza-horizon-5-banner.jpg',
+    imageUrl: '/forza-horizon-5-banner.png',
     title: 'Forza Horizon 5: Premium Edition',
     description:
       'Your Ultimate Horizon Adventure awaits! Explore the vibrant and ever-evolving open world landscapes of Mexico with limitless, fun driving action in hundreds of the world’s greatest cars. The Premium Edition includes early access to the full game, allowing you to play four days early beginning November 5, 2021. Also included are the Welcome Pack, Car Pass, VIP Membership, and two game expansions when they become available.',
+    price: 359.9,
+    platformsAvaliable: [2, 1],
+  },
+]
+
+const mockGameCards = [
+  {
+    id: 1,
+    imageUrl: '/gta-6.png',
+    title: 'Grand Theft Auto VI',
+    description:
+      'Grand Theft Auto VI heads to the state of Leonida, home to the neon-soaked streets of Vice City and beyond in the biggest, most immersive evolution of the Grand Theft Auto series yet.',
+    price: 359.9,
+    platformsAvaliable: [2, 1],
+  },
+  {
+    id: 2,
+    imageUrl: '/horizon-forbidden-west.png',
+    title: 'Horizon Forbidden West',
+    description:
+      'Horizon Forbidden West continues Aloy’s story as she moves west to a far-future America to brave a majestic, but dangerous frontier where she’ll face awe-inspiring machines and mysterious new threats.',
+    price: 359.9,
+    platformsAvaliable: [2, 1],
+  },
+  {
+    id: 3,
+    imageUrl: '/forza-horizon-5.png',
+    title: 'Forza Horizon 5: Premium Edition',
+    description:
+      'Your Ultimate Horizon Adventure awaits! Explore the vibrant and ever-evolving open world landscapes of Mexico with limitless, fun driving action in hundreds of the world’s greatest cars. The Premium Edition includes early access to the full game, allowing you to play four days early beginning November 5, 2021. Also included are the Welcome Pack, Car Pass, VIP Membership, and two game expansions when they become available.',
+    price: 359.9,
+    platformsAvaliable: [2, 1],
+  },
+  {
+    id: 4,
+    imageUrl: '/minecraft.png',
+    title: 'Minecraft',
+    description: `Minecraft focuses on allowing the player to explore, interact with, and modify a dynamically-generated map made of one-cubic-meter-sized blocks. In addition to blocks, the environment features plants, mobs, and items. Some activities in the game include mining for ore, fighting hostile mobs, and crafting new blocks and tools by gathering various resources found in the game. The game's open-ended model allows players to create structures, creations, and artwork on various multiplayer servers or their single-player maps. Other features include redstone circuits for logic computations and remote actions, minecarts and tracks, and a mysterious underworld called the Nether. A designated but completely optional goal of the game is to travel to a dimension called the End, and defeat the ender dragon.`,
+    price: 359.9,
+    platformsAvaliable: [2, 1],
+  },
+  {
+    id: 5,
+    imageUrl: '/zelda-breath-of-the-wild.png',
+    title: 'The Legend of Zelda: Breath of the Wild',
+    description:
+      'In this 3D open-world entry in the Zelda series, Link is awakened from a deep slumber without his past memories in the post-apocalyptic Kingdom of Hyrule, and sets off on a journey to defeat the ancient evil Calamity Ganon. Link treks, climbs and glides through fields, forests and mountain ranges while meeting and helping friendly folk and defeating enemies in order to gather up the strength to face Ganon.',
     price: 359.9,
     platformsAvaliable: [2, 1],
   },
---
+++ src/components/game-vertical-card.tsx
@@ -0,0 +1,33 @@
+import { Game } from '@/types/game'
+
+type GameVerticalCardProps = {
+  game: Game
+}
+
+export function GameVerticalCard({ game }: GameVerticalCardProps) {
+  return (
+    <div className="w-full min-w-[150px] flex flex-col gap-2">
+      <div className="aspect-[3/4] overflow-hidden rounded-xl">
+        <img
+          src={game.imageUrl}
+          alt="game"
+          className="size-full object-cover"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <span className="text-base font-bold text-white">{game.title}</span>
+
+        <p className="line-clamp-3 text-xs text-zinc-400">{game.description}</p>
+
+        <span className="min-w-max text-sm text-white font-semibold">
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+            maximumFractionDigits: 2,
+          }).format(game.price)}
+        </span>
+      </div>
+    </div>
+  )
+}
