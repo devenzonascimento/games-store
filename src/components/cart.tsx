e1a1162a9fcbaeb95472126018ba332ffc322cc6
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-05-13 02:25:14 -0300

    feat: atualizar lógica do carrinho para suportar novos tipos de desconto e exibir mensagem quando o carrinho estiver vazio

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BanknoteIcon, Trash2Icon, XIcon } from 'lucide-react'
+import { BanknoteIcon, ShoppingCartIcon, Trash2Icon, XIcon } from 'lucide-react'
 import {
   Sheet,
   SheetClose,
@@ -14,8 +14,6 @@ import { useCartStore } from '@/store/cart-store'
 import { DiscountType } from '@/types/product'
 import { formatPercentage } from '@/helpers/format-percentage'
 import { formatCurrency } from '@/helpers/format-currency'
-import { useToast } from '@/hooks/use-toast'
-import { ToastAction } from './ui/toast'
 
 export function Cart() {
   const {
@@ -27,9 +25,7 @@ export function Cart() {
     isCartOpen,
     closeCart,
   } = useCartStore()
-
-  const { toast } = useToast()
-
+console.log(getTotalPrice())
   return (
     <Sheet open={isCartOpen} onOpenChange={closeCart}>
       <SheetContent className="w-[350px] sm:w-[540px] py-4 px-4 flex flex-col gap-6">
@@ -43,63 +39,75 @@ export function Cart() {
           </SheetClose>
         </SheetHeader>
 
-        <div className="flex flex-col gap-4 overflow-y-auto no-scrollbar">
-          {items.map(item => (
-            <CartItem
-              key={item.id}
-              id={item.game.id}
-              title={item.game.title}
-              imageUrl={item.game.imageUrl}
-              platform={item.game.platformsAvailable?.[0]}
-              price={item.price}
-              discount={item.discount / 100}
-              discountType={item.discountType}
-              onRemove={() => removeItem(item.id)}
-            />
-          ))}
-        </div>
+        {items.length > 0 && (
+          <>
+            <div className="flex flex-col gap-4 overflow-y-auto no-scrollbar">
+              {items.map(item => (
+                <CartItem
+                  key={item.id}
+                  id={item.game.id}
+                  title={item.game.title}
+                  imageUrl={item.game.imageUrl}
+                  platform={item.game.platformsAvailable?.[0]}
+                  price={item.price}
+                  discount={item.discountValue}
+                  discountType={item.discountType}
+                  onRemove={() => removeItem(item.id)}
+                />
+              ))}
+            </div>
 
-        <ul className="w-full flex flex-col gap-2">
-          <li className="px-2 w-full flex items-center justify-between">
-            <span className="text-base text-zinc-300">Cart total</span>
-            <span className="text-base font-bold text-zinc-200">
-              {formatCurrency(getTotalPrice())}
-            </span>
-          </li>
+            <ul className="w-full flex flex-col gap-2">
+              <li className="px-2 w-full flex items-center justify-between">
+                <span className="text-base text-zinc-300">Cart total</span>
+                <span className="text-base font-bold text-zinc-200">
+                  {formatCurrency(getTotalPrice())}
+                </span>
+              </li>
 
-          <li className="px-2 w-full flex items-center justify-between">
-            <span className="text-base text-zinc-300">Discount total</span>
-            <span className="text-base font-bold text-zinc-200">
-              {formatCurrency(-getTotalDiscount())}
-            </span>
-          </li>
+              <li className="px-2 w-full flex items-center justify-between">
+                <span className="text-base text-zinc-300">Discount total</span>
+                <span className="text-base font-bold text-zinc-200">
+                  {formatCurrency(-getTotalDiscount())}
+                </span>
+              </li>
 
-          <div className="w-full h-[1px] bg-zinc-500" />
+              <div className="w-full h-[1px] bg-zinc-500" />
 
-          <li className="px-2 w-full flex items-center justify-between">
-            <span className="text-base text-zinc-300">Subtotal</span>
-            <span className="text-lg font-bold text-white">
-              {formatCurrency(getTotalPrice() - getTotalDiscount())}
-            </span>
-          </li>
-        </ul>
+              <li className="px-2 w-full flex items-center justify-between">
+                <span className="text-base text-zinc-300">Subtotal</span>
+                <span className="text-lg font-bold text-white">
+                  {formatCurrency(getTotalPrice() - getTotalDiscount())}
+                </span>
+              </li>
+            </ul>
 
-        <button
-          type="button"
-          className="w-full p-2 flex items-center justify-center gap-2 text-lg font-medium text-white bg-transparent border border-white rounded-lg hover:opacity-70"
-          onClick={clearCart}
-        >
-          <Trash2Icon className="size-6 shrink-0" />
-          Clear cart
-        </button>
+            <button
+              type="button"
+              className="w-full p-2 flex items-center justify-center gap-2 text-lg font-medium text-white bg-transparent border border-white rounded-lg hover:opacity-70"
+              onClick={clearCart}
+            >
+              <Trash2Icon className="size-6 shrink-0" />
+              Clear cart
+            </button>
 
-        <button
-          type="button"
-          className="w-full p-2 flex items-center justify-center gap-2 text-lg font-medium text-zinc-950 bg-white rounded-lg hover:opacity-70"
-        >
-          <BanknoteIcon className="size-6 shrink-0" />
-          Procced to checkout
-        </button>
+            <button
+              type="button"
+              className="w-full p-2 flex items-center justify-center gap-2 text-lg font-medium text-zinc-950 bg-white rounded-lg hover:opacity-70"
+            >
+              <BanknoteIcon className="size-6 shrink-0" />
+              Procced to checkout
+            </button>
+          </>
+        )}
+
+        {items.length === 0 && (
+          <div className="py-10 flex flex-col items-center justify-center gap-4 text-center text-zinc-400">
+            <ShoppingCartIcon className="text-zinc-500 size-24" />
+            <p className="text-lg">Your cart is empty</p>
+            <p className="text-sm">Add some games to start shopping!</p>
+          </div>
+        )}
       </SheetContent>
     </Sheet>
   )
@@ -158,25 +166,28 @@ export function CartItem({
         </div>
 
         <div className="mt-auto w-full flex items-center gap-2">
-          <div className="h-4 px-1.5 flex items-center justify-center rounded-full bg-emerald-800">
-            {discountType === DiscountType.Percentage && (
-              <span className="text-xs font-medium text-white">
-                {formatPercentage(-discount)}
+          {discount && (
+            <>
+              <div className="h-4 px-1.5 flex items-center justify-center rounded-full bg-emerald-800">
+                {discountType === DiscountType.Percentage && (
+                  <span className="text-xs font-medium text-white">
+                    {formatPercentage(-discount)}
+                  </span>
+                )}
+
+                {discountType === DiscountType.Fixed && (
+                  <span className="text-xs font-medium text-white">
+                    {formatCurrency(-discount)}
+                  </span>
+                )}
+              </div>
+              <span className="mr-auto line-through text-sm text-zinc-500">
+                {formatCurrency(price)}
               </span>
-            )}
+            </>
+          )}
 
-            {discountType === DiscountType.Value && (
-              <span className="text-xs font-medium text-white">
-                {formatCurrency(-discount)}
-              </span>
-            )}
-          </div>
-
-          <span className="line-through text-sm text-zinc-500">
-            {formatCurrency(price)}
-          </span>
-
-          <span className="ml-auto text-base font-bold text-white">
+          <span className="text-base font-bold text-white">
             {formatCurrency(finalPrice)}
           </span>
         </div>
--- src/store/cart-store.ts
+++ src/store/cart-store.ts
@@ -1,13 +1,15 @@
+import { GameCartItem } from '@/types/game'
 import { DiscountType, ProductWithGame } from '@/types/product'
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 type CartState = {
   isCartOpen: boolean
   openCart: () => void
   closeCart: () => void
 
-  items: ProductWithGame[]
-  addItem: (product: ProductWithGame) => void
+  items: ProductWithGame<GameCartItem>[]
+  addItem: (product: ProductWithGame<GameCartItem>) => void
   removeItem: (id: number) => void
   clearCart: () => void
   getTotalItems: () => number
@@ -15,39 +17,87 @@ type CartState = {
   getTotalDiscount: () => number
 }
 
-export const useCartStore = create<CartState>((set, get) => ({
-  isCartOpen: false,
-  openCart: () => set({ isCartOpen: true }),
-  closeCart: () => set({ isCartOpen: false }),
+// export const useCartStore = create<CartState>((set, get) => ({
+//   isCartOpen: false,
+//   openCart: () => set({ isCartOpen: true }),
+//   closeCart: () => set({ isCartOpen: false }),
 
-  items: [],
-  addItem: product => {
-    const items = get().items
-    const existingItem = items.find(i => i.id === product.id)
+//   items: [],
+//   addItem: product => {
+//     const items = get().items
+//     const existingItem = items.find(i => i.id === product.id)
 
-    if (!existingItem) {
-      set({
-        items: [...items, product],
-      })
-    }
-  },
-  removeItem: id => {
-    const item = get().items.find(i => i.id === id)
-    if (item) {
-      set({
-        items: get().items.filter(i => i.id !== id),
-      })
-    }
-  },
-  clearCart: () => set({ items: [] }),
-  getTotalItems: () => get().items.length,
-  getTotalPrice: () => get().items.reduce((acc, i) => acc + i.price, 0),
-  getTotalDiscount: () =>
-    get().items.reduce((acc, i) => {
-      if (i.discountType === DiscountType.Percentage) {
-        return acc + (i.price * i.discount) / 100
-      }
+//     if (!existingItem) {
+//       set({
+//         items: [...items, product],
+//       })
+//     }
+//   },
+//   removeItem: id => {
+//     const item = get().items.find(i => i.id === id)
+//     if (item) {
+//       set({
+//         items: get().items.filter(i => i.id !== id),
+//       })
+//     }
+//   },
+//   clearCart: () => set({ items: [] }),
+//   getTotalItems: () => get().items.length,
+//   getTotalPrice: () => get().items.reduce((acc, i) => acc + i.price, 0),
+//   getTotalDiscount: () =>
+//     get().items.reduce((acc, i) => {
+//       if (i.discountType === DiscountType.Percentage) {
+//         return acc + (i.price * i.discount) / 100
+//       }
 
-      return acc + i.price
-    }, 0),
-}))
+//       return acc + i.price
+//     }, 0),
+// }))
+
+export const useCartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+      isCartOpen: false,
+      openCart: () => set(state => ({ ...state, isCartOpen: true })),
+      closeCart: () => set(state => ({ ...state, isCartOpen: false })),
+
+      items: [],
+      addItem: product => {
+        const items = get().items
+        const existingItem = items.find(i => i.id === product.id)
+
+        if (!existingItem) {
+          set({
+            items: [...items, product],
+          })
+        }
+      },
+      removeItem: id => {
+        const item = get().items.find(i => i.id === id)
+        if (item) {
+          set({
+            items: get().items.filter(i => i.id !== id),
+          })
+        }
+      },
+      clearCart: () => set({ items: [] }),
+      getTotalItems: () => get().items.length,
+      getTotalPrice: () => get().items.reduce((acc, i) => acc + i.price, 0),
+      getTotalDiscount: () =>
+        get().items.reduce((acc, i) => {
+          if (!i.discountValue) {
+            return acc
+          }
+          
+          if (i.discountType === DiscountType.Percentage) {
+            return acc + (i.price * i.discountValue) / 100
+          }
+
+          return acc + i.price
+        }, 0),
+    }),
+    {
+      name: 'cart-storage',
+    },
+  ),
+)
