cd2110c09c91740c366e7efe37ab8c79e5efa440
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-05-25 21:14:53 -0300

    refactor(base-url): corrigir lógica de construção da URL de produção

--- src/helpers/base-url.ts
+++ src/helpers/base-url.ts
@@ -1,4 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    ? process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+      : process.env.NEXT_PUBLIC_BASE_URL
     : process.env.NEXT_PUBLIC_BASE_URL
