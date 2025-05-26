3401ec8500ad237a90d684f7c2404c145cd3bf7f
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-05-25 21:03:18 -0300

    refactor(base-url): adicionar protocolo HTTPS à URL de produção

--- src/helpers/base-url.ts
+++ src/helpers/base-url.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
     : process.env.NEXT_PUBLIC_BASE_URL
